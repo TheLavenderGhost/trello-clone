1f63b0880defb62c6c4ac9327ae82a089742a740
Katarzyna Sajchta <62062768+TheLavenderGhost@users.noreply.github.com>
2023-12-10 14:44:02 +0100

    commit on enter

--- src/components/addNewItem/NewItemForm.tsx
+++ src/components/addNewItem/NewItemForm.tsx
@@ -5,6 +5,7 @@ import {
   NewItemInput,
 } from './styles';
 import { useInputFocus } from '../../utils/useInputFocus';
+import { Key } from '../../common/enums';
 
 type NewItemFormProps = {
   onAdd(text: string): void,
@@ -14,12 +15,21 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [ text, setText ] = useState('');
   const inputRef = useInputFocus();
 
+  const handleAddText = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === Key.Enter) {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         ref={inputRef}
         value={text}
         onChange={e => setText(e.target.value)}
+        onKeyDown={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>
         Create
